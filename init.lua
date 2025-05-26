7692829e939ce7f65ec83982b1845642da35e233
srujaniyengar <srujanparthasarathyiyengar@gmail.com>
2025-05-26 12:22:42 +0530

    chore(.): update remaining files

--- init.lua
+++ init.lua
@@ -3,4 +3,6 @@ require("lazy").setup("plugins")
 require("options") -- If you have one
 require("plugins")
 require("keymaps")
+require("lsp") -- assuming your lsp files are in lua/lsp/init.lua etc.
+
 pcall(require, "lsp") -- will not error if not present
