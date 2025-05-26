c1755193cb9620fb41fef77bf88b9e50bbeede8c
srujaniyengar <srujanparthasarathyiyengar@gmail.com>
2025-05-26 12:04:24 +0530

    docs(keymaps)

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,6 +1,7 @@
 return {
   { "folke/lazy.nvim", version = "*" },
 
+  -- Theme
   {
     "marko-cerovac/material.nvim",
     config = function()
@@ -8,46 +9,82 @@ return {
     end,
   },
 
+  -- File Explorer with Git integration
   {
     "nvim-tree/nvim-tree.lua",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-      require("nvim-tree").setup()
+      require("nvim-tree").setup {
+        git = {
+          enable = true,
+          ignore = false, -- show .gitignored files
+        },
+        renderer = {
+          highlight_git = true,
+          icons = {
+            show = {
+              git = true,
+            },
+          },
+        },
+      }
     end,
   },
 
+  -- Git signs in buffer (gutter)
   {
-    "neovim/nvim-lspconfig",
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup {
+        signs = {
+          add          = { text = "│" },
+          change       = { text = "│" },
+          delete       = { text = "_" },
+          topdelete    = { text = "‾" },
+          changedelete = { text = "~" },
+          untracked    = { text = "┆" },
+        },
+        current_line_blame = true,
+      }
+    end,
   },
 
+  -- LSP
+  { "neovim/nvim-lspconfig" },
+
+  -- Terminal
   {
     "akinsho/toggleterm.nvim",
     version = "*",
     config = function()
-      require("toggleterm").setup({
+      require("toggleterm").setup {
         size = function(term)
           if term.direction == "vertical" then
-            return math.floor(vim.o.columns * 0.4) -- 50% width for vertical terminals
+            return math.floor(vim.o.columns * 0.4)
           else
-            return 15 -- default for horizontal
+            return 15
           end
         end,
         shade_terminals = false,
-        direction = "float", -- default, overridden by keymaps for vertical
+        direction = "float",
         start_in_insert = true,
         close_on_exit = false,
-      })
+      }
     end,
   },
 
+  -- Tabs
   {
     "romgrk/barbar.nvim",
-    dependencies = { "nvim-web-devicons" },
-    init = function() vim.g.barbar_auto_setup = false end,
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    init = function()
+      vim.g.barbar_auto_setup = false
+    end,
     opts = {},
     version = "^1.0.0",
   },
 
+  -- Telescope
   {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.5",
@@ -57,8 +94,10 @@ return {
     end,
   },
 
+  -- Formatter
   { "stevearc/conform.nvim", opts = {} },
 
+  -- Commenting
   {
     "numToStr/Comment.nvim",
     config = function()
@@ -66,6 +105,7 @@ return {
     end,
   },
 
+  -- Which-key popup
   {
     "folke/which-key.nvim",
     config = function()
